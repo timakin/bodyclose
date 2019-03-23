82ab6e1e1432d297b4ec3e0421819d8c561b6d24
Seiji Takahashi <timaki.st@gmail.com>
2019-03-23 20:10:54 +0900

    add: passes

---
+++ passes/bodyclose/bodyclose.go
@@ -0,0 +1,22 @@
+package bodyclose
+
+import (
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/buildssa"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "bodyclose",
+	Doc:  Doc,
+	//Run:  new(runner).run,
+	Requires: []*analysis.Analyzer{
+		buildssa.Analyzer,
+	},
+}
+
+const (
+	Doc = "bodyclose checks whether HTTP response body is closed successfully"
+
+	nethttpPath = "net/http"
+)
+
